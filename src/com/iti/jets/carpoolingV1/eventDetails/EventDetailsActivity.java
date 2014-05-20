389cea1c8ee3c87a6e1f5660a650ac49f5dd2b26
SarahMohamed268 <sara.mohamed269@gmail.com>
2014-05-20 01:57:20 -0700

    Event Details Missing pkg

---
+++ src/com/iti/jets/carpoolingV1/eventDetails/EventDetailsActivity.java
@@ -0,0 +1,67 @@
+package com.iti.jets.carpoolingV1.eventDetails;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.iti.jets.carpoolingV1.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.TextView;
+
+
+public class EventDetailsActivity extends Fragment{
+
+	TextView eventName;
+	TextView eventDate;
+	TextView eventFrom;
+	TextView eventTo;
+	TextView noOfSlots;
+	TextView comments;
+	TextView members;
+	View rootView ;
+	EventDetialsController controller;
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+		 
+	//	 super.onCreateView(inflater, container, savedInstanceState);
+		 
+         rootView = inflater.inflate(R.layout.activity_event_details, container, false);
+        
+         eventName= (TextView) rootView.findViewById(R.id.eventName);
+         eventDate= (TextView) rootView.findViewById(R.id.eventDate);
+         eventFrom= (TextView) rootView.findViewById(R.id.eventFrom);
+         eventTo  = (TextView) rootView.findViewById(R.id.eventTo);
+         noOfSlots= (TextView) rootView.findViewById(R.id.noOfSlots);
+         members  = (TextView) rootView.findViewById(R.id.members);
+         comments = (TextView) rootView.findViewById(R.id.comments);
+         
+
+         eventName.setText("");
+         eventDate.setText("");
+         eventFrom.setText("");
+         eventTo.setText("");
+         noOfSlots.setText("");
+         members.setText("");
+         comments.setText("");
+         
+         setHasOptionsMenu(true);
+         
+         controller = new EventDetialsController(this);
+         
+         JSONObject obj = new JSONObject();
+         try {
+			obj.put("idEvent", 1);
+			controller.retrieveEventHandler(obj.toString());
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+         
+        return rootView;
+    }
+
+	
+}
